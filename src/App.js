e8886d5bba94a683a982ffa6e372095422e3d75e
TadjievCodes <muhammad2008.08@mail.ru>
2021-05-23 03:48:27 -0400

    Added nameFilterFunction into the App

--- src/App.js
+++ src/App.js
@@ -7,12 +7,26 @@ import ContentFilter from "./components/contentFilter/ContentFilter";
 function App() {
     
   const [studentData, setStudentData] = useState([]);
+  const [nameFilter, setNameFilter] = useState([]);
+
+  const nameFilterFunction = str => {
+    let newNameFilter = [];
+    studentData.map(student => {
+      const fullName = `${student.firstName} ${student.lastName}`.toLowerCase();
+      if (fullName.includes(str)) {
+        newNameFilter.push(student);
+      }
+    });
+    setNameFilter(newNameFilter);
+  };
+
   async function fetchUrl(url) {
     const response = await fetch(url);
     const newStudentData = await response.json();
     console.log(newStudentData);
     setStudentData(newStudentData.students);
   }
+  
   useEffect(() => {
     fetchUrl(`https://api.hatchways.io/assessment/students`);
   }, []); 
