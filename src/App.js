5123e2804cc71f1580b5c895b26e4ccc84af7fc8
TadjievCodes <muhammad2008.08@mail.ru>
2021-05-23 01:20:38 -0400

    challenge 1 completed, fetch data from JSON API in the app and display it on the screen (get average grade )

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import StudentDataCard from "./components/studentCard/StudentCard";
+import "./App.css";
+
+
 
 function App() {
-  return (
-        
+    
+  const [studentData, setStudentData] = useState([]);
+  async function fetchUrl(url) {
+    const response = await fetch(url);
+    const newStudentData = await response.json();
+    console.log(newStudentData);
+    setStudentData(newStudentData.students);
+  }
+  useEffect(() => {
+    fetchUrl(`https://api.hatchways.io/assessment/students`);
+  }, []); 
+
+   
+   return (
+   
+   <div>
+      {studentData.map((student, index) => {
+        function findAverage(array) {
+          let sum = 0;
+          for (let i = 0; i < array.length; i++) {
+            sum += parseInt(array[i]);
+          }
+          let average = sum / array.length;
+          return average;
+        }
+        const averageGrade = findAverage(student.grades);
+        return (
+          <StudentDataCard
+            img={student.pic}
+            firstName={student.firstName}
+            lastName={student.lastName}
+            email={student.email}
+            company={student.company}
+            skill={student.skill}
+            averageGrade={averageGrade}
+          />
+        );
+      })}
+    </div>
   );
 }
+
+ 
 
 export default App;
